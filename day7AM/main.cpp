f04c3c52f813ed78777f2ba0615739ffe9eab769
Martin Leblancs <martin.leblancs@epitech.eu>
2022-02-09 18:59:53 +0100

    [ADD] ALL my CPP_POOL

---
+++ day7AM/main.cpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2022
+** day7AM
+** File description:
+** main
+*/
+
+#include "Federation.hpp"
+#include "WarpSystem.hpp"
+#include "Borg.hpp"
+    
+int main ()
+{
+Federation :: Starfleet :: Ship UssKreog (289 , 132 , "Kreog" , 6) ;
+Federation :: Starfleet :: Captain James ( "James T. Kirk" ) ;
+Federation :: Starfleet :: Ensign Ensign ( "Pavel Chekov" ) ;
+WarpSystem :: QuantumReactor QR ;
+WarpSystem :: QuantumReactor QR2 ;
+WarpSystem :: Core core (& QR ) ;
+WarpSystem :: Core core2 (& QR2 ) ;
+UssKreog . setupCore (& core ) ;
+UssKreog . checkCore () ;
+UssKreog . promote (& James ) ;
+Borg :: Ship Cube ;
+Cube . setupCore (& core2 ) ;
+Cube . checkCore () ;
+return 0;
+}
